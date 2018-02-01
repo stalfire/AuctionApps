ac2fd055c4a4dc65ed4643b02cba81047ad2fe87
nextacademy <engchienhowe@hotmail.com>
2018-02-01 11:59:43 +0800

    add seed and change category data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Database for user
+User.create(name: "stal", email: "stal@hotmail.com", password: "stal", role: 1)
+
+user = {}
+user['password'] = 'stal'
+
+ActiveRecord::Base.transaction do
+  20.times do 
+    user['name'] = Faker::Name.name
+    user['email'] = Faker::Internet.email
+
+    User.create(user)
+  end
+end 
+
+# Seed Recipes
+auction = {}
+uids = []
+User.all.each { |u| uids << u.id }
+
+ActiveRecord::Base.transaction do
+  40.times do 
+    auction['title'] = Faker::Zelda.item
+    auction['category'] = ["House", "Vehicle", "Electronics", "Sport", "Clothing", "Other"].sample
+    auction['user_id'] = uids.sample
+
+    Auction.create(auction)
+  end
+end
